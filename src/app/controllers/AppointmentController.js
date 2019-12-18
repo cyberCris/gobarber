f0414208df122194fa17de77cc6a40e197d24646
Cristiano Soares <cristiano.soares@adlim.local>
2019-12-18 10:09:32 -0300

    adding function cancel appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
 import Appointment from '../models/Appointment';
@@ -127,6 +127,28 @@ class AppointmentController {
                 },
             ],
         });
+
+        return res.json(appointment);
+    }
+
+    async delete(req, res) {
+        const appointment = await Appointment.findByPk(req.params.id);
+
+        if (appointment.user_id !== req.userId)
+            return res.status(401).json({
+                error: 'You dont have permission to delete this appointment',
+            });
+
+        const dateWithSub = subHours(appointment.date, 2);
+
+        if (isBefore(dateWithSub, new Date()))
+            return res.status(401).json({
+                error: 'You can only cancel appointments within 2 hours',
+            });
+
+        appointment.canceled_at = new Date();
+
+        await appointment.save();
 
         return res.json(appointment);
     }
